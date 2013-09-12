85f8a843872bccade8a2b7f7ddf0d634616761b0
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-12 16:49:07 -0400

    Now it decides to recognize the files.
    Github has started working.

---
+++ JS/javascript.js
@@ -0,0 +1,73 @@
+//console.log('test');          /**Allows us to comment on the javascript**/
+//
+//var initialValue= 2;
+//
+//console.log(initialValue);
+//
+//initialValue = initialValue+5;
+//
+//console.log(initialValue)
+//
+//initialValue = initialValue-5;
+//
+//console.log(initialValue)
+//
+//initialValue = initialValue*5;
+//
+//console.log(initialValue)
+//
+//initialValue/=3; /**Same as previous, just shorter and cleaner**/
+//
+//console.log(initialValue)
+//
+
+//var length;
+//var width;
+//var height;
+//var area;
+//
+
+//length=20;
+//width=30;
+//height=40;
+//area=length*width*height;
+//
+
+var radius=3;
+
+circ= radius*2*Math.PI;
+
+console.log(circ)
+
+//console.log(area);                   /**Short hand way to show the result**/
+//console.log(length*width*height)     /**Long version**/
+
+
+//console.log(Math.PI)
+
+var salary= prompt('Set the initial Salary')*1;
+var raise=1.05;
+var firstyear= salary*.05;
+
+console.log(firstyear)
+
+var secondyear = firstyear*raise;
+
+console.log(secondyear)
+
+var thirdyear= secondyear*raise;
+
+
+console.log(thirdyear)
+
+var initialSalary =prompt('Set the Initial Value')
+
+initialSalary= initialSalary+ initialSalary *.05;
+
+initalSalary= initialSalary+ initialSalary *.05;
+
+initialSalary= initialSalary+ initialSalary *.05;
+
+
+
+
