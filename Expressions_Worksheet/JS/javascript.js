d21f221355dd5eea6fefb57181dc59b46d1436e5
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-12 21:34:34 -0400

    Added Three parts of the Workshee

--- Expressions_Worksheet/JS/javascript.js
+++ Expressions_Worksheet/JS/javascript.js
@@ -5,3 +5,66 @@
  * Time: 8:29 PM
  * To change this template use File | Settings | File Templates.
  */
+//Emmanuel Barreto // Thursday September 12, 2013 //
+
+//Dog Years
+
+//Sparky's Age
+
+var sparkysDob = 2007;
+
+var age = 2013 - sparkysDob;
+
+var humanYears = "In human years Sparky is " + age +  " years old.";
+
+console.log(); console.log(humanYears);
+
+age*=7;
+
+var dogYears = "However, dog years Sparky is " + age +  " years old. \n";
+
+
+console.log(dogYears)  ;
+
+
+//Slice of Pie part 1
+
+var sliceOfpie = "Slice of Pie Part I \n"
+
+console.log(sliceOfpie);
+
+var pizzaSlices = 12;
+
+var partyPeople = 5;
+
+var pizzaOrder = 1;
+
+var result = "Each person gets "+ pizzaSlices / partyPeople * pizzaOrder + " pizza slices.";
+
+console.log(result+"\n");
+
+
+//Slice of Pie part 2
+
+var sliceOfpieII = "Slice of Pie Part II \n"  ;
+
+console.log(sliceOfpieII) ;
+
+
+var leftOverpizza = (pizzaSlices % partyPeople);
+
+var sparkyGets = "Sparky gets " + leftOverpizza + " slices of pizza." ;
+
+console.log(sparkyGets+"\n");
+
+//Average Shopping Bill
+
+var groceryShopping = [ 22, 60, 100, 70, 40];
+
+
+
+
+
+
+
+
