8ddec321e111a0719957b497f828da6e58490670
ebhemmanuel <akkisan@fullsail.edu>
2013-09-17 05:21:26 -0700

    Making small changes.

--- Conditionals_Worksheet/JS/javascript.js
+++ Conditionals_Worksheet/JS/javascript.js
@@ -17,7 +17,7 @@ function myDivision () {
             if(myWeight >249)                    {returnGrade= approvalMessage[0]}
        else if(myWeight <248)                    {returnGrade= approvalMessage[1]}
 
-    return returnGrade;
+       return returnGrade;
 
 }
 
