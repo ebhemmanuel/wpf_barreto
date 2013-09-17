e42a27e8c4ef83d35ab595cc308b7f59be4b642e
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-17 02:27:09 -0400

    Adding the tempature conversion.

--- Conditionals_Worksheet/JS/javascript.js
+++ Conditionals_Worksheet/JS/javascript.js
@@ -9,11 +9,36 @@
 
 // Stuff your Face I
 
+function Division () {
+    var myWeight          = prompt('Enter your weight(lb):');   // =prompt('Set the Initial Value')
+    var approvalMessage   = [ "The competitor qualifies for the heavyweight division.", "The competitor needs to gain some weight!"];
+    var returnGrade       = '';
+
+            if(myWeight >249)                    {returnGrade= approvalMessage[0]}
+       else if(myWeight <248)                    {returnGrade= approvalMessage[1]}
+
+    return returnGrade; }
+
+console.log(Division());
 
 
 
 // Celsius to Fahrenheit converter
 
+//   F to C Deduct 32, then multiply by 5, then divide by 9
+//   C to F Multiply by 9, then divide by 5, then add 32
+
+function myTemperature () {
+    var theTemp           = prompt('Enter the tempature value in Celsius:');   // =prompt('Set the Initial Value')
+    var tempConversion    = [ theTemp-=32*5/9, theTemp*=9/5+35 ];
+    var returnTemperature = '';
+
+    if(theTemp ()+"F")                    {returnTemperature= tempConversion[0]}
+    else if(theTemp ()+"C")                    {returnTemperature= tempConversion[1]}
+
+    return returnTemperature; }
+
+console.log(myTemperature());
 
 
 
@@ -24,11 +49,11 @@
 // Grade Letter Calculator
 
 function myMathGrade () {
-    var numberGrade   = prompt('Enter the Grade # for math:');   // =prompt('Set the Initial Value')
+    var numberGrade = prompt('Enter the Grade # for math:');   // =prompt('Set the Initial Value')
     var gradeSystem = [ "A", "B", "C", "D", "F"];
     var returnGrade = '';
 
-    if(numberGrade >89)                         {returnGrade= gradeSystem[0]}
+         if(numberGrade >89)                    {returnGrade= gradeSystem[0]}
     else if(numberGrade >79 && numberGrade<90 ) {returnGrade= gradeSystem[1]}
     else if(numberGrade >69 && numberGrade<80 ) {returnGrade= gradeSystem[2]}
     else if(numberGrade >59 && numberGrade<70 ) {returnGrade= gradeSystem[3]}
@@ -39,11 +64,11 @@ function myMathGrade () {
 console.log(myMathGrade());
 
 function myEnglishGrade () {
-    var numberGrade   = prompt('Enter the Grade # for English:');
+    var numberGrade = prompt('Enter the Grade # for English:');
     var gradeSystem = [ "A", "B", "C", "D", "F"];
     var returnGrade = '';
 
-    if(numberGrade >89)                         {returnGrade= gradeSystem[0]}
+         if(numberGrade >89)                    {returnGrade= gradeSystem[0]}
     else if(numberGrade >79 && numberGrade<90 ) {returnGrade= gradeSystem[1]}
     else if(numberGrade >69 && numberGrade<80 ) {returnGrade= gradeSystem[2]}
     else if(numberGrade >59 && numberGrade<70 ) {returnGrade= gradeSystem[3]}
@@ -55,11 +80,11 @@ function myEnglishGrade () {
 console.log(myEnglishGrade());
 
 function myHistoryGrade () {
-    var numberGrade   = prompt('Enter the Grade # for History:');
+    var numberGrade = prompt('Enter the Grade # for History:');
     var gradeSystem = [ "A", "B", "C", "D", "F"];
     var returnGrade = '';
 
-    if(numberGrade >89)                         {returnGrade= gradeSystem[0]}
+         if(numberGrade >89)                    {returnGrade= gradeSystem[0]}
     else if(numberGrade >79 && numberGrade<90 ) {returnGrade= gradeSystem[1]}
     else if(numberGrade >69 && numberGrade<80 ) {returnGrade= gradeSystem[2]}
     else if(numberGrade >59 && numberGrade<70 ) {returnGrade= gradeSystem[3]}
