1a28232c74297f84053a9f4856103f146eaf4934
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-20 23:04:04 -0400

    Uploading preset files, and progress so far

---
+++ Expressions Assignment/Expressions_Industry/JS/javascript.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: emmanuelbarreto
+ * Date: 9/20/13
+ * Time: 9:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Emmanuel Barreto // Thursday September 20, 2013 //
+
+// User must meet the criteria of being at least level 45, and gear level being at least lvl 40.
+
+function myGear  () {
+    var levelCap
+
+
+
+
