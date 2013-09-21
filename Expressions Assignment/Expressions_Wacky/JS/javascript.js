4b80aec3b19ff201e1d71e8dbf2281023e2f6a05
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-21 00:04:34 -0400

    adding result

--- Expressions Assignment/Expressions_Wacky/JS/javascript.js
+++ Expressions Assignment/Expressions_Wacky/JS/javascript.js
@@ -13,6 +13,11 @@ function calorieIntake () {
     var yourWeight     = parseInt (prompt('What is your weight? (lb):'));
     var   myCarbs      = parseInt(prompt ('How many carbs do you intake a day:'));
     var  myAge         = parseInt(prompt('Tell us your age:'))
+    var equationTime   = yourWeight/myCarbs*myAge
+
+    var  result        = "I'm "+myAge+" years old, I weigh "+yourWeight+" and I intake "+myCarbs+" amount of carbs a day. Making it a total of "+equationTime+" calories.";
+
+
 
 
 
