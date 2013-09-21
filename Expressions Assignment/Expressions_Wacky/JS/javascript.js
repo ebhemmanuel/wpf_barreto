d713f107fa18b07a7e636810d7855e3e892a1ef3
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-21 00:06:52 -0400

    Added a higher value

--- Expressions Assignment/Expressions_Wacky/JS/javascript.js
+++ Expressions Assignment/Expressions_Wacky/JS/javascript.js
@@ -13,7 +13,7 @@ function calorieIntake () {
     var yourWeight     = parseInt (prompt('What is your weight? (lb):'));
     var   myCarbs      = parseInt(prompt ('How many carbs do you intake a day:'));
     var  myAge         = parseInt(prompt('Tell us your age:'))
-    var equationTime   = yourWeight/myCarbs*myAge
+    var equationTime   = yourWeight/myCarbs*myAge*100
 
     var  result        = "I'm "+myAge+" years old, I weigh "+yourWeight+" and I intake "+myCarbs+" amount of carbs a day. Making it a total of "+equationTime+" calories.";
 
