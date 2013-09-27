7e5031212958a2bbce832e9f7f0b0e45d2b7edf1
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-27 17:16:18 -0400

    conditionals first var

---
+++ Conditional Assigment/Conditionals_Industry/js/javascript.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: emmanuelbarreto
+ * Date: 9/27/13
+ * Time: 5:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * Created with JetBrains WebStorm.
+ * User: emmanuelbarreto
+ * Date: 9/12/13
+ * Time: 8:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Emmanuel Barreto // Thursday September 12, 2013 //
+
+
+
+
+    var myBankbalance          = parseInt(prompt('How much money do you have in your account?'))        // Added first prompt
+
