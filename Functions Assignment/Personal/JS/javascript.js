9113d2a801e7c0026963b99f57e5172cb473ffd7
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-09-26 20:08:16 -0400

    Start of second code

---
+++ Functions Assignment/Personal/JS/javascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Emmanuel Barreto
+ * Date: 9/26/13
+ * Time: 9:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Emmanuel Barreto // Thursday September 26, 2013 //
+
+var   mySuit= parseInt(prompt()) ;                                    //Prompts user for value.
